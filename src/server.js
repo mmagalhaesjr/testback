fc98259ca834240d66077ddac8f5bde1f8ca020c
mmagalhaesjr <magalhaesmarcosjr@gmail.com>
2024-04-09 15:46:00 -0300

    update

--- src/server.js
+++ src/server.js
@@ -1,61 +1,101 @@
 import express from "express";
 import cors from "cors";
 
+import dotenv from "dotenv";
+dotenv.config()
+
 const server = express().use(cors());
 server.use(express.json())
 const PORTA = 4001;
 
-const carros = [
-    { marca: 'volkswagen', modelo: 'rete', carro: 'gol' },
-    { marca: 'volkswagen', modelo: 'sedã', carro: 'jetta' },
-    { marca: 'chevrolet', modelo: 'sedã', carro: 'camaro' },
-    { marca: 'chevrolet', modelo: 'rete', carro: 'onix' },
-    { marca: 'chevrolet', modelo: 'sedã', carro: 'prisma' },
-    { marca: 'fiat', modelo: 'sedã', carro: 'siena' },
-    { marca: 'fiat', modelo: 'rete', carro: 'palio' },
+
+
+
+
+const usuarios = [
+    { nome: "marcos", email: 'marcos@marcos', senha: 123 }
+]
+
+const todasTarefas = [
+    { id: 1, nomeTarefa: "criar blog", data: '02/03/2024' },
+    { id: 1, nomeTarefa: "criar landing pages", data: '02/07/2024' },
+    { id: 2, nomeTarefa: "criar video", data: '02/07/2024' },
 ]
 
 
-//========================================================================
-server.get("/carros", (req, res) => {
-
+server.post("/cadastrar", async (req, res) => {
+    const usuario = req.body
 
     try {
-        res.send(carros);
+        const cadastro = await usuarios.find(obj => obj.email === usuario.email)
+
+        if (!usuario.nome || !usuario.email || !usuario.senha) {
+            return res.status(422).send('Preencha os campos corretamente')
+        }
+
+        if (cadastro) return res.status(404).send('Usuario já cadastrado')
+
+        usuarios.push(usuario)
+        res.status(201).send('Usuario cadastrado com sucesso.')
+
+
     } catch (error) {
-        res.status(500).send('Erro ao encontrar o carro: ' + error.message);
+        res.status(500).send('Erro no servidor')
     }
 })
-//----------------------------------------------------------------------
-server.post("/carros", (req, res) => {
-    const body = req.body
-    
-    try {
-        carros.push(body);
-        res.status(201).send('carro cadastrado com sucesso');
 
+// ----------------------------------------------------------------------------
+
+server.post("/login", async (req, res) => {
+    const usuario = req.body
+
+    try {
+        if (!usuario.email || !usuario.senha) {
+            return res.status(422).send('Preencha os campos corretamente')
+        }
+
+        const login = await usuarios.find(obj => obj.email == usuario.email && obj.senha == usuario.senha)
+
+        if (!login) return res.status(404).send('Usuario não encontrado')
+
+        res.status(200).send('Usuario entrou')
     } catch (error) {
-        res.status(500).send('Erro ao cadastrar o carro: ' + error.message);
+        res.status(500).send('Erro no servidor')
     }
+
 })
-//----------------------------------------------------------------------
-server.put("/carros", (req, res) => {
-    const { marca } = req.body;
 
-    const carrosFiltrados = carros.filter(objeto =>
-        objeto.marca.toLowerCase() === marca.toLowerCase()
-    );
+// ----------------------------------------------------------------------------
 
+server.get("/todasTarefas", (req, res) => {
+   
     try {
-        res.send(carrosFiltrados);
+        return res.send(todasTarefas)
     } catch (error) {
-        res.status(500).send('Erro: ' + error.message);
+        res.status(500).send('Erro no servidor')
     }
 
-});
-//========================================================================
+})
+
+// ----------------------------------------------------------------------------
+
+server.get("/minhasTarefas/:id", async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const tarefasUsuario = await todasTarefas.filter(obj => obj.id == id)
+        if (tarefasUsuario.length === 0) return res.send('Tarefa de usuario não encontrada')
+
+        return res.send(tarefasUsuario.reverse())
+    } catch (error) {
+        res.status(500).send('Erro no servidor')
+    }
 
 
+})
+
+
+// ----------------------------------------------------------------------------
 
 
 server.listen(PORTA, () => {
